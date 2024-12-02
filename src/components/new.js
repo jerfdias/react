06db1ed69b60f916404193e59d081c78ff2faba4
jerfdias <jerfdias@gmail.com>
2024-12-02 18:25:53 -0300

    atualizando a pagina

--- src/components/new.js
+++ src/components/new.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import Image from '../assets/img.jpeg';
 import Logo from '../assets/volks.png';
-
+import '../components/new.css';
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 function New() {
   return (
 
     <div>
       
      <main className='principalContent'>
-     <img src={Image}/>
+    
      <form action='POST' className='formulario'>
-      <img src={Logo}/>
-     <label for="nomeCliente">Nome:</label>
+      <img src={Logo} className='Logo'id='Logo'/>
+      <FaUser className='iconeUser'id="iconeUser" /><label for="nomeCliente">Nome:</label>
+
+    
      <input type="email"id="nomeCliente" placeholder='digite seu email'></input>
+     <FaLock />
      <label for="senhaCliente">Senha:</label>
      <input type="password" id='senhaCliente' className='senhaCliente' required  placeholder='digite sua senha aqui'></input>
      <button type>Login</button>
      </form>
+    
        
 
       </main>
